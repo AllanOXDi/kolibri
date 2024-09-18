77a4a867e039b19f451a7a964b138fc2d343de1d
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2024-09-18 17:46:32 +0300

    WIP

--- packages/kolibri-common/composables/useCoachMetadataTags.js
+++ packages/kolibri-common/composables/useCoachMetadataTags.js
@@ -1,1 +1,57 @@
+import { ref } from 'kolibri.lib.vueCompositionApi';
+import { ContentNodeResource } from 'kolibri.resources';
 
+export function useCoachMetadataTags(contentNode) {
+  const tags = ref([]);
+
+  const getCategoryTags = () => {
+    if (!contentNode.categories) return [];
+    return contentNode.categories.slice(0, 3);
+  };
+
+  const getLevelTags = () => {
+    if (!contentNode.levels) return [];
+    return contentNode.levels.slice(0, 3);
+  };
+
+  const getLanguageTag = () => {
+    if (!contentNode.languages) return [];
+    return contentNode.languages.length > 1 ? ['Multiple languages'] : [contentNode.languages[0]];
+  };
+
+  const getActivityTag = () => {
+    if (!contentNode.activities) return [];
+    return contentNode.activities.length > 1 ? ['Multiple learning activities'] : [contentNode.activities[0]];
+  };
+
+  const getDurationTag = () => {
+    if (!contentNode.duration) return [];
+    return [contentNode.duration];
+  };
+
+  const getSpecificCategoryTag = () => {
+    if (!contentNode.categories) return [];
+    const specificCategories = contentNode.categories.filter(category => category.specific);
+    return specificCategories.length > 0 ? 
+    [specificCategories[specificCategories.length - 1].specific] 
+    : [contentNode.categories[0]];
+  };
+
+  if (contentNode.type === 'channel' || contentNode.type === 'folder') {
+    tags.value = [
+      ...getCategoryTags(),
+      ...getLevelTags(),
+      ...getLanguageTag(),
+    ].slice(0, 7);
+  } else if (contentNode.type === 'resource') {
+    tags.value = [
+      ...getActivityTag(),
+      ...getDurationTag(),
+      ...getLevelTags(),
+      ...getSpecificCategoryTag(),
+      ...getLanguageTag(),
+    ].slice(0, 3);
+  }
+
+  return tags;
+}
